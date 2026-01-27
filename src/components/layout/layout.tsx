0f397632edb068ae38a21151a8dedbb353a07825
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-01-26 22:13:22 -0300

    feat(image): add background images   Added background images for the call-to-action and support-sections. Fixed the position of the store icon in the call-to-action section.

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -26,7 +26,7 @@ export function Layout({
   return (
     <div className={`${inter.className} ${ptSansCaption.className} relative flex min-h-screen flex-col bg-gray-700 font-inter`}>
       <Header/>
-      <main className="flex-1 flex flex-col mt-10 mb-12">
+      <main className="flex-1 flex flex-col mt-10">
         {children}
       </main>
       <Footer/>
