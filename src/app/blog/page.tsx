dfb8764398e5061c5166e3e6d09302c4bf96efa6
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-04 16:31:25 -0300

    feat(blog): create metadata   Creating metatada for Blog.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,29 @@
+import type { Metadata } from "next";
 import { BlogList } from "@/templates/blog/blog-list";
 import { allPosts } from "contentlayer/generated";
+
+
+export const metadata: Metadata = {
+  title: "Blog",
+  description: "Dicas e estratégias para impulsionar seu negócio",
+  robots: "index, follow",
+  openGraph: {
+    title: "Blog",
+    description: "Dicas e estratégias para impulsionar seu negócio",
+    url: "https://next-blog-site-ten.vercel.app/og-image.jpg",
+    siteName: "Site.set",
+    locale: "pt_BR",
+    type: "website",
+    images: [
+      {
+        url: "https://next-blog-site-ten.vercel.app/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Blog",
+      },
+    ]
+  }
+};
 
 
 export default function BlogPage() {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
+import type { Metadata } from "next"; 
 import { Layout } from "@/components/layout";
 import "@/styles/globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Site.set",
+  description: "Venda seus produtos como afiliado em um único lugar",
+};
+
 
 export default function DashboardLayout({
   children,
