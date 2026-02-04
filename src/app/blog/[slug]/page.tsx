f20064a1d64e5391502c918c2d0f96f40d7dd759
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-04 11:49:08 -0300

    feat(post-page): adding static config   Adding de static config to next to render static page.

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -8,10 +8,17 @@ type PostPageProps = {
   }>
 }
 
+export const revalidate = 60;
+
+export async function generateStaticParams() {
+  return allPosts.map((post) => ({
+    slug: post.slug
+  }));
+};
 
 export default async function PostPage({
   params,
-  
+
 }: PostPageProps) {
   const { slug } = await params;
   const post = allPosts.find((post) => post.slug === slug);
