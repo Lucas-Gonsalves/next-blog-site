023b5e9100f3928ab16f3c430855e47f8f0a385c
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-04 12:31:17 -0300

    fix(page-blog): remove props   Removing props from the page function.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,17 +2,8 @@ import { BlogList } from "@/templates/blog/blog-list";
 import { allPosts } from "contentlayer/generated";
 
 
-
-type BlogPageProps = {
-  posts: any
-};
-
-export default function BlogPage({
-  posts
-}: BlogPageProps) {
+export default function BlogPage() {
   const sortedPosts = allPosts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
-
-
   return (
     <div>
       <BlogList posts={sortedPosts}/>
